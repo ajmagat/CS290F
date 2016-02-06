1bdc484fd1eeefbeb6c349b3625e891c0a0c1eb6
otownsend92 <otownsend92@gmail.com>
2016-02-06 15:15:33 -0800

    Basic accelerometer class and activity built

--- TBA/app/src/main/java/com/rao/tba/MainActivity.java
+++ TBA/app/src/main/java/com/rao/tba/MainActivity.java
@@ -64,6 +64,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    public void startAccelerometer(View view) {
+        Intent accelIntent = new Intent(this, AccelerometerActivity.class);
+        startActivity(accelIntent);
+    }
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
